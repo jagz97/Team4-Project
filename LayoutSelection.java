8ae60b966477e555357d8de8876ba09755b832b4
jagz97 <68725607+jagz97@users.noreply.github.com>
2022-05-04 19:50:32 -0700

    Update LayoutSelection.java

--- LayoutSelection.java
+++ LayoutSelection.java
@@ -12,9 +12,82 @@ class LayoutSelection extends JFrame {
 
     private BoardLayout layout;
 
+    public JFrame frame2() {
+       
+        JFrame frame = new JFrame();
+        frame.setLayout(null);
 
+        layout = new BoardLayout2();
+
+
+
+
+
+        DrawPit pit = new DrawPit();
+        pit.setBackground(Color.yellow);
+        pit.setLocation(150,275);
+        pit.setSize(650,250);
+
+        JPanel newPanel = new JPanel();
+        JLabel label2 = new JLabel("Player x turn");
+        JButton button3 = new JButton("undo");
+        JButton button4 = new JButton("end");
+        newPanel.add(label2);
+        newPanel.add(button3);
+        newPanel.add(button4);
+
+        button3.addActionListener(e -> {
+            //Undo button action listener here
+        });
+
+        button4.addActionListener(e -> {
+            //end gama button action listener here
+
+        });
+
+
+
+
+
+
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("How many Stones per pit?");
+        JButton button = new JButton("3");
+        JButton button1 = new JButton("4");
+        panel.add(label);
+        panel.add(button);
+        button.addActionListener(e -> {
+            //three stone per pit selection button
+            frame.remove(panel);
+            frame.add(newPanel);
+            newPanel.setLocation(250,700);
+            newPanel.setSize(500,100);
+
+            frame.validate();
+
+        });
+        panel.add(button1);
+        button1.addActionListener(e -> {
+            // 4 stones per pit selection button
+        });
+
+        panel.setSize(500,100);
+        panel.setLocation(250,700);
+        frame.add(panel);
+
+        frame.add(pit);
+        frame.add((Component) layout);
+
+        frame.setSize(1000,800);
+        frame.setVisible(true);
+
+        return frame;
+    }
+    
+    
+    
     public JFrame frame1() {
-
+        //This in vertical layout not functioning.Use frame 2 for testing
         JFrame frame = new JFrame();
         frame.setLayout(null);
 
@@ -62,78 +135,6 @@ class LayoutSelection extends JFrame {
         frame.setSize(1000,800);
         frame.setVisible(true);
 
-
-        return frame;
-    }
-
-
-    public JFrame frame2() {
-        //This is vertical layout currently not functional
-        JFrame frame = new JFrame();
-        frame.setLayout(null);
-
-        layout = new BoardLayout2();
-
-
-
-
-
-        DrawPit pit = new DrawPit();
-        pit.setBackground(Color.yellow);
-        pit.setLocation(150,275);
-        pit.setSize(650,250);
-
-        JPanel newPanel = new JPanel();
-        JLabel label2 = new JLabel("Player x turn");
-        JButton button3 = new JButton("undo");
-        JButton button4 = new JButton("end");
-        newPanel.add(label2);
-        newPanel.add(button3);
-        newPanel.add(button4);
-
-        button3.addActionListener(e -> {
-            //Undo button action listener here
-        });
-
-        button4.addActionListener(e -> {
-            //end gama button action listener here
-
-        });
-
-
-
-
-
-
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("How many Stones per pit?");
-        JButton button = new JButton("3");
-        JButton button1 = new JButton("4");
-        panel.add(label);
-        panel.add(button);
-        button.addActionListener(e -> {
-            frame.remove(panel);
-            frame.add(newPanel);
-            newPanel.setLocation(250,700);
-            newPanel.setSize(500,100);
-
-            frame.validate();
-
-        });
-        panel.add(button1);
-        button1.addActionListener(e -> {
-            // 4 stones per pit selection
-        });
-
-        panel.setSize(500,100);
-        panel.setLocation(250,700);
-        frame.add(panel);
-
-        frame.add(pit);
-        frame.add((Component) layout);
-
-        frame.setSize(1000,800);
-        frame.setVisible(true);
 
         return frame;
     }
